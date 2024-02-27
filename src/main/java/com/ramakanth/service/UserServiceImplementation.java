45834cbe932f1b4f5c58d61683b4bca91afefe50
ramlukky <42038473+ramlukky@users.noreply.github.com>
2024-02-26 22:41:09 -0800

    Update UserServiceImplementation.java

--- src/main/java/com/ramakanth/service/UserServiceImplementation.java
+++ src/main/java/com/ramakanth/service/UserServiceImplementation.java
@@ -1,8 +1,6 @@
 package com.ramakanth.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.ramakanth.entity.UserEntity;
@@ -20,25 +18,8 @@ public class UserServiceImplementation implements UserService {
 	return (userEntity.getUserId()==null);
 	}
 
-	@Autowired
-	public JavaMailSender javaMailSender;
-
-	public Boolean sendEmail(UserEntity userEntity){
-
-	SimpleMailMessage smm=new SimpleMailMessage();
-
-	smm.setFrom("a.ramakanth.recruiter@gmail.com");
-	smm.setTo(userEntity.getUserEmail());
-	String body="Hi "+userEntity.getUserName()+", you have succesfully Registered: Thank you";
-	smm.setText(body);
-	smm.setSubject("Registration Sucessful");
-
-javaMailSender.send(smm);
-
-System.out.println("mail sent;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;");
-
-return true;
-	}
+	
+	
 
 public String findByUserEmail(String userEmail){
 return userRepo.findByUserEmail(userEmail);
